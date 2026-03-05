27cbf65f49eb44552c2dcede74cce9fae424dbe3
Abdullah Al Mamun <abdullahmn3399@gmail.com>
2026-03-05 05:21:42 +0000

    dock completed

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,10 +1,12 @@
 import { dockApps } from "#constants";
+import useWindowStore from "#store/window.js";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef } from "react";
 import { Tooltip } from 'react-tooltip'
 
 const Dock = () => {
+    const { openWindow, closeWindow, focusWindow, windows } = useWindowStore();
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -56,7 +58,18 @@ const Dock = () => {
     }, [])
 
 
-    const toggleApp = (app) => {}
+    const toggleApp = (app) => {
+        if(!app.canOpen) return ;
+
+        const window = windows[app.id];
+
+        if(window.isOpen){
+            closeWindow(app.id)
+        }else{
+            openWindow(app.id)
+        }
+
+    }
     return (
         <section id="dock">
             <div ref={dockRef} className="dock-container">
